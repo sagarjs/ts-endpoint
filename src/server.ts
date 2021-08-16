c1d2a8d443db1a96177b6d5dedcaed6bd351dfff
Sagar Sodah <sagarsodah@Sagars-MacBook-Pro.local>
2021-08-16 11:49:00 +0530

    added port env variable

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './app';
 import * as fs from 'fs';
 import * as http2 from 'spdy';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const serverOptions = {
     key: fs.readFileSync('cert/ssl.key'),
