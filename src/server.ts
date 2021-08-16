95c37f5217b7a98312f9a58dbae5d2c0c4bedca9
Sagar Sodah <sagarsodah@Sagars-MacBook-Pro.local>
2021-08-16 12:47:30 +0530

    Rolled back to plain express http

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,9 @@ import * as http2 from 'spdy';
 
 const port = process.env.PORT || 3000;
 
-const serverOptions = {
-    key: fs.readFileSync('cert/ssl.key'),
-    cert: fs.readFileSync('cert/ssl.crt')
-};
-const server = http2.createServer(serverOptions, app);
-server.listen(port);
-server.on('listening', () => {
-    console.info(`ts-node-boilerplace on https://localhost:${port}`);
-    console.info(`ts-node-boilerplace api documentation on https://localhost:${port}/api/doc`);
+const server = app
+
+server.listen(port, () => {
+    console.info(`tserver running  on https://localhost:${port}`);
+    console.info(`api documentation on https://localhost:${port}/api/doc`);
 });
